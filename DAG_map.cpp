3bb97f9d268144757587d7164c2a58ce2699b4a5
GRADYCHOI <chldls12@kookmin.ac.kr>
2022-05-26 22:38:57 +0900

    critical path algorithm complite

--- DAG_map.cpp
+++ DAG_map.cpp
@@ -11,8 +11,12 @@
 
 using namespace std;
 
-int dfs(int delay_time, int count, int num, vector<vector<int>> run, int e[], vector<int> delay);
+int dfs(int delay_time, int count, int num, vector<vector<int>> run, int e[]);
 int critical_path(int num, vector<vector<int>> run, int e[]);
+int gb_count = 0;
+vector<int> delay(10, 0);
+vector<int> path(1);
+vector<vector<int>> critical_path_dag(10, vector<int>(5,0));
 
 int main(int argc, char *argv[]) {
     int number;
@@ -53,36 +57,34 @@ int main(int argc, char *argv[]) {
 	}
     cout << "Find Critical Path" << endl;
     critical_path(number, runnable, e);
+    int critical_delay = delay[0];
+    int critical_dag = 0;
+
+    for (int i = 0; i < 10; i++) {
+        if (delay[i] > critical_delay) { 
+            critical_delay = delay[i];
+            critical_dag = i;
+        }        
+    }
+    printf("critical delay = %dms, critical path num : %d\ncritical path : ", critical_delay, critical_dag);
+    for (int i = 1; i <= 5; i++) {
+        if (critical_path_dag[critical_dag][i] != 0) printf("%d -> ", critical_path_dag[critical_dag][i]);
+    }
+    printf("\n");
     
 	return 0;
 }
 
-int critical_path(int num, vector<vector<int>> run, int e[]) {
-    vector<int> delay;
-    
+int critical_path(int num, vector<vector<int>> run, int e[]) { 
     int critical_delay = 0, critical_count = 1, next_dag = 1;
-    int b = dfs(critical_delay, critical_count, num, run, e, delay);
-    delay.push_back(b);
-
-    for (auto d : delay) {
-        cout << d << " ";
-    }
-    cout << endl;
-
+    int b = dfs(critical_delay, critical_count, num, run, e);
     return 0;
 }
 
-int dfs(int delay_time, int count, int num, vector<vector<int>> run, int e[], vector<int> delay) {
-    //count++;
-    //if (count > num) {
-    //    printf("SDFSDF\n\n");
-        //run.clear
-    //    return delay_time;// count 말고 프랙티스하게 경로이동을 잡는게 좋을듯, 예외처리도
-    //끝까지 말고 경로 끝나면으로.
-    //각 패스의 딜레이를 배열에 넣고 가장 오래걸리는거 비교, 그러려면 해당 딜레이의 dag도 알아야함
-    //}
-    if (count < num) { // i값을 놓는 것도 필요함 ex) next_dag = i 이런식으로
+int dfs(int delay_time, int count, int num, vector<vector<int>> run, int e[]) {
+    if (count < num) {
         printf("current runnable : %d\n", count);
+        path.push_back(count);
         delay_time += e[count];
         cout << "delay = " << delay_time <<endl;
         for (int i = 0; i < num; i++) {
@@ -90,18 +92,22 @@ int dfs(int delay_time, int count, int num, vector<vector<int>> run, int e[], ve
 
             else {
                 cout << count << "'s dag : " << run[count][i] << endl;
-                delay_time = dfs(delay_time, run[count][i], num, run, e, delay); // 여기가 문제인듯
-                
+                cout << delay_time << endl;
+                delay_time = dfs(delay_time, run[count][i], num, run, e);  
             }
         }
         printf("\n");
     }
     else {
         printf("last runnable : %d\n", count);
+        path.push_back(count);
         delay_time += e[count];
+        copy(path.begin(), path.end(), critical_path_dag[gb_count].begin());
         cout << "delay = " << delay_time <<endl;
-        delay.push_back(delay_time);
-        return delay_time;
+        delay[gb_count] = delay_time;
+        gb_count ++;
     }
+    path.pop_back();
+    delay_time -= e[count];
     return delay_time;
 }
